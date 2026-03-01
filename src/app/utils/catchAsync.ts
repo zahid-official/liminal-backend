9b95d90ef1a497f1df678765ece09fdca37229e8
zahid-official <zahid.official8@gmail.com>
2026-03-02 04:41:40 +0600

    Add sendResponse utility for standardized API responses

--- src/app/utils/catchAsync.ts
+++ src/app/utils/catchAsync.ts
@@ -4,8 +4,8 @@ const catchAsync = (fn: RequestHandler) => {
   return async (req: Request, res: Response, next: NextFunction) => {
     try {
       await fn(req, res, next);
-    } catch (err) {
-      next(err);
+    } catch (error) {
+      next(error);
     }
   };
 };
