614346edafde68c5e987819cacd0ca188b19e796
zahid-official <zahid.official8@gmail.com>
2026-03-03 23:30:01 +0600

    Add getTokens utility for JWT access and refresh token generation

---
+++ src/app/utils/getTokens.ts
@@ -0,0 +1,31 @@
+import envVars from "../configs/index.js";
+import type { IUser } from "../modules/user/user.interface.js";
+import { generateJWT } from "./jwt.js";
+
+// getTokens Function
+const getTokens = (user: Partial<IUser>) => {
+  // Create JWT Payload
+  const jwtPayload = {
+    userId: user._id,
+    email: user.email,
+    role: user.role,
+  };
+
+  // Generate Access Token
+  const accessToken = generateJWT(
+    jwtPayload,
+    envVars.JWT.ACCESS_SECRET,
+    envVars.JWT.ACCESS_EXPIRESIN,
+  );
+
+  // Generate Refresh Token
+  const refreshToken = generateJWT(
+    jwtPayload,
+    envVars.JWT.REFRESH_SECRET,
+    envVars.JWT.REFRESH_EXPIRESIN,
+  );
+
+  return { accessToken, refreshToken };
+};
+
+export default getTokens;
