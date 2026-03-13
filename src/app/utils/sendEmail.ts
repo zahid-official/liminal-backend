4934474eff43a69bf8f19a444c32f82508235a43
zahid-official <zahid.official8@gmail.com>
2026-03-14 00:47:16 +0600

    Refactor sendEmail utility: add type definitions and enhance error handling

--- src/app/utils/sendEmail.ts
+++ src/app/utils/sendEmail.ts
@@ -1,5 +1,16 @@
 import { createTransport } from "nodemailer";
 import envVars from "../config/index.js";
+import AppError from "../error/AppError.js";
+import { httpStatus } from "../import/index.js";
+import type { Attachment } from "nodemailer/lib/mailer/index.js";
+
+// Define email options interface
+interface EmailOptions {
+  to: string;
+  subject: string;
+  html: string;
+  attachments?: Attachment[];
+}
 
 // Configure SMTP transporter
 const transporter = createTransport({
@@ -11,3 +22,29 @@ const transporter = createTransport({
     pass: envVars.SMTP.PASS,
   },
 });
+
+// Function to send email
+const sendEmail = async (options: EmailOptions): Promise<void> => {
+  try {
+    const mailOptions = {
+      from: envVars.SMTP.FROM,
+      to: options.to,
+      subject: options.subject,
+      html: options.html,
+      attachments: options?.attachments?.map((attachment) => ({
+        filename: attachment.filename,
+        content: attachment.content,
+        contentType: attachment.contentType,
+      })),
+    };
+
+    await transporter.sendMail(mailOptions);
+  } catch (error: any) {
+    throw new AppError(
+      httpStatus.INTERNAL_SERVER_ERROR,
+      error.message || "Error sending email",
+    );
+  }
+};
+
+export default sendEmail;
